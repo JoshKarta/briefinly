80eed249dddab7c3974a6aec48afe933b4f1a22e
Josh Kartadwirja <josh.kartadiwirja@kabinetpresident.gov.sr>
2024-08-13 11:44:25 -0300

    feat: add call to action block

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -2,12 +2,11 @@ import PricingBlock from "@/blocs/pricing-block";
 import AboutBlock from "@/blocs/about-block";
 import { HeroBlock } from "@/blocs/hero-block";
 import MarqueeBlock from "@/blocs/marquee-block";
+import ActionBlock from "@/blocs/action-block";
 
 export default function Home() {
-
   return (
-    <main className="flex flex-col lg:pt-36 dark:bg-black">
-
+    <main className="flex flex-col dark:bg-black lg:pt-36">
       {/* Hero */}
       <div className="w-full" id="home">
         <HeroBlock />
@@ -28,6 +27,10 @@ export default function Home() {
         <PricingBlock />
       </div>
 
+      {/* Action */}
+      <div className="w-full py-2 md:py-12">
+        <ActionBlock />
+      </div>
     </main>
   );
 }
