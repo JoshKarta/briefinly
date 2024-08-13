270113c3691f3f2b9934566d272712ebbf7c831a
Josh Kartadwirja <josh.kartadiwirja@kabinetpresident.gov.sr>
2024-08-13 00:55:51 -0300

    refactor: add dark mode text for FormInput component

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -4,7 +4,7 @@ import { Label } from './ui/label'
 export default function FormInput({ children, name }: { children: React.ReactNode, name: string }) {
     return (
         <div className='flex flex-col gap-1'>
-            <Label htmlFor={name} className='capitalize'>{name}</Label>
+            <Label htmlFor={name} className='capitalize dark:text-zinc-100'>{name}</Label>
             {children}
         </div>
     )
