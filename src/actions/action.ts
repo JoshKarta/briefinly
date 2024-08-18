ccba6737c5a578e648faaae6e9f03a0c61976ee5
Josh <karta.josh.natin@gmail.com>
2024-08-18 15:35:51 -0300

    feat (dashboard): add new form inputs

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,27 +1,36 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
-import { revalidatePath } from "next/cache";
 import { v4 as uuidV4 } from "uuid";
 
 export async function createLetter(formData: FormData) {
   const supabase = createClient();
+  const title = formData.get("title") as string;
   const text = formData.get("text") as string;
   const user_id = formData.get("user_id") as string;
+  const confetti = formData.get("confetti") === "true"; // Changed this line
+  const confetti_type = formData.get("confetti_type") as string;
+  const confetti_emoji = formData.get("confetti-emoji") as string;
   const letter_id = uuidV4();
 
   // Set the user_id as a session variable
   await supabase.rpc("set_claim", { name: "app.user_id", value: user_id });
 
-  const { error } = await supabase
-    .from("letters")
-    .insert({ text, user_id, letter_id });
+  const { error } = await supabase.from("letters").insert({
+    title,
+    text,
+    user_id,
+    letter_id,
+    confetti,
+    confetti_type,
+    confetti_emoji: confetti_type === "custom" ? confetti_emoji : null,
+  });
 
   if (error) {
     console.error("Supabase error:", JSON.stringify(error, null, 2));
     throw new Error(`Failed to create letter: ${error.message}`);
   }
 
-  revalidatePath("/dashboard/letters");
+  // revalidatePath("/dashboard/letters");
   return { success: true };
 }
--- src/app/dashboard/letters/letter-form.tsx
+++ src/app/dashboard/letters/letter-form.tsx
@@ -3,25 +3,32 @@ import { createLetter } from '@/actions/action'
 import FormInput from '@/components/FormInput'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Switch } from '@/components/ui/switch'
 import { Textarea } from '@/components/ui/textarea'
 import { useUser } from '@clerk/nextjs'
 import { useRef, useState } from 'react'
 import { toast } from 'sonner'
 
-export default function LetterForm({ handleDialog }: { handleDialog: () => void }) {
+export default function LetterForm({ handleDialog, onLetterCreated }: { handleDialog: () => void, onLetterCreated: () => void }) {
     const formRef = useRef<HTMLFormElement>(null)
     const [loading, setLoading] = useState(false)
+    const [confettiType, setConfettiType] = useState<string>('basic')
+    const [confettiEnabled, setConfettiEnabled] = useState(false)
     const { user } = useUser()
 
     return (
         <form action={async (formData: FormData) => {
             try {
+                setLoading(true)
+                formData.set('confetti', confettiEnabled.toString());
                 await createLetter(formData)
                 formRef.current?.reset()
                 toast.success(`Letter created successfully`)
-                setLoading(true)
                 handleDialog()
+                onLetterCreated()
             } catch (error) {
+                // toast.error("Something went wrong. Please try again later")
                 toast.error(error as string)
                 console.log(error)
             } finally {
@@ -37,6 +44,36 @@ export default function LetterForm({ handleDialog }: { handleDialog: () => void 
             <FormInput name='text'>
                 <Textarea name='text' placeholder='I love Twix Chocolate' />
             </FormInput>
+            <div className='grid grid-cols-6 gap-4'>
+                <FormInput name='confetti'>
+                    <Switch
+                        checked={confettiEnabled}
+                        onCheckedChange={setConfettiEnabled}
+                    />
+                </FormInput>
+                <FormInput name='confetti Type' className='col-span-3'>
+                    <Select
+                        name='confetti_type'
+                        onValueChange={(value) => setConfettiType(value)}
+                        disabled={!confettiEnabled}
+                    >
+                        <SelectTrigger className="flex-1">
+                            <SelectValue placeholder="Basic" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {['basic', 'stars', 'custom'].map((item, i) => (
+                                <SelectItem value={item} key={i} className='capitalize'>{item}</SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </FormInput>
+
+                {confettiType === 'custom' && confettiEnabled && (
+                    <FormInput name='Confetti emoji' className='col-span-2'>
+                        <Input type='text' name="confetti-emoji" placeholder='🦄' />
+                    </FormInput>
+                )}
+            </div>
             <Button type='submit' disabled={loading}>Save</Button>
         </form>
     )
